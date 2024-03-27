d94b436a51070399cf2be8e33794fdfe0397fb0c
Alejandro Ruete <aleruete@users.noreply.github.com>
2024-03-27 09:36:36 +0100

    import galah

--- R/onLoad.R
+++ R/onLoad.R
@@ -4,10 +4,12 @@
 
 #' Set-up for sbdi4r during loading
 #' @noRd
+#' @import galah
 #' @keywords Internal
 .onLoad <- function(libname, pkgname) {
   packageStartupMessage("This is a wrapper for the package galah-R designed to work with data hosted by the Swedish Biodiversity Data Infrastructure (SBDI).")
   if (pkgname == "sbdi4r2") {
+    requireNamespace("galah")
     sbdi_config(atlas = "Sweden") # to cache defaults
   }
 }
